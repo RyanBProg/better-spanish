5c1bf878ba731a7bebcc850ccc686bd2bc4ed453
Ryan <ryanbprog@gmail.com>
2024-08-07 12:05:39 +0200

    verb game completed

--- app/components/verb-conj/VerbInputCard.tsx
+++ app/components/verb-conj/VerbInputCard.tsx
@@ -1,87 +1,92 @@
 "use client";
 
-import { FormEvent, useState } from "react";
+import { useState } from "react";
+import { UseFormRegister, FieldErrors } from "react-hook-form";
+import { TFormInput } from "./VerbConj";
 
-type AnswerStatus = boolean | undefined;
+type TVerb = {
+  prefix?: string | string[];
+  spanish: string;
+  english: string;
+  type?: string;
+};
 
 type TVerbCard = {
   classes: string;
-  title: string;
-  answer: string;
+  verb: TVerb;
+  register: UseFormRegister<TFormInput>;
+  errors: FieldErrors;
+  isSubmitted: boolean;
 };
 
-export default function VerbInputCard({ classes, title, answer }: TVerbCard) {
-  const [isCorrect, setIsCorrect] = useState<AnswerStatus>(undefined);
+export default function VerbInputCard({
+  classes,
+  verb,
+  register,
+  errors,
+  isSubmitted,
+}: TVerbCard) {
   const [showAnswer, setShowAnswer] = useState(false);
-  const [userAnswer, setUserAnswer] = useState("");
 
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-
-    if (userAnswer.toLowerCase() === answer.toLowerCase()) {
-      setIsCorrect(true);
-      setShowAnswer(false);
+  const getInputName = () => {
+    if (Array.isArray(verb.prefix)) {
+      return `${verb.prefix[0]}_${verb.spanish}`;
+    } else if (!verb.prefix) {
+      return `${verb.spanish}`;
     } else {
-      setIsCorrect(false);
-      setShowAnswer(false);
+      return `${verb.prefix}_${verb.spanish}`;
     }
   };
 
+  const inputName = getInputName();
+
   return (
-    <form
-      className={`grid grid-cols-[min-content_1fr] grid-rows-[min-content_1fr] gap-x-4 border-white ${classes}`}
-      onSubmit={(e) => handleSubmit(e)}
-      autoComplete="off">
+    <div
+      className={`grid grid-cols-[min-content_1fr] grid-rows-[min-content_1fr] gap-x-4 border-white py-6 ${classes}`}>
       <label
-        htmlFor="yo"
+        htmlFor={inputName}
         className="font-medium text-wrap table-caption w-min [word-spacing:999rem] row-span-2">
-        {title}
+        {Array.isArray(verb.prefix) ? verb.prefix.join(" ") : verb.prefix}
       </label>
       <div className="flex relative overflow-hidden">
         <input
-          id="yo"
+          id={inputName}
           type="text"
           className="bg-white p-2 text-base px-2 py-1 flex-grow min-w-0 h-fit"
-          value={userAnswer}
-          onChange={(e) => setUserAnswer(e.target.value)}
-          disabled={isCorrect}
+          {...register(inputName, {
+            required: "Field is required",
+            validate: (value) =>
+              value.toLocaleLowerCase() === verb.spanish.toLocaleLowerCase() ||
+              "Incorrect answer",
+          })}
         />
         <p
           className={`absolute right-0 py-1 px-2 bg-orange-300 text-white text-center whitespace-nowrap slide-transition ${
             showAnswer ? "slide-in" : "slide-out"
           }`}>
-          {answer}
+          {verb.spanish}
         </p>
       </div>
-
       <div className="relative flex justify-end gap-1">
         <button
           type="button"
           className={`${
-            showAnswer && "bg-orange-400 text-white"
-          } border-[1px] border-t-0 border-orange-400 text-orange-400 text-xs rounded-b-md w-[100px] p-2 h-fit transition-colors hover:brightness-110`}
-          onClick={() => setShowAnswer((prev) => !prev)}
-          disabled={isCorrect}>
+            showAnswer && "bg-orange-300"
+          } bg-orange-400 text-white text-xs w-[95px] py-[3px] h-fit transition-colors hover:brightness-110`}
+          onClick={() => setShowAnswer((prev) => !prev)}>
           {showAnswer ? "Hide Answer" : "Show Answer"}
         </button>
-        <button
-          disabled={isCorrect}
-          className={`${isCorrect && "bg-gray-300"} ${
-            !isCorrect && "bg-green-500 hover:brightness-110"
-          } transition-colors text-white rounded-b-md text-xs p-2 h-fit`}>
-          Submit
-        </button>
-        {isCorrect === false && (
-          <p className="text-red-500 text-sm font-semibold absolute right-[170px] top-2">
-            Try Again!
+        {errors[inputName] && (
+          <p className="text-red-500 text-sm font-semibold absolute right-[110px] top-1">
+            {errors[inputName]?.message?.toString()}
           </p>
         )}
-        {isCorrect && (
-          <p className="text-green-600 text-sm font-semibold absolute right-[170px] top-2">
-            Correct Answer!
+        {isSubmitted && !errors[inputName] && (
+          <p className="text-green-500 text-sm font-semibold absolute right-[110px] top-1">
+            Correct
           </p>
         )}
       </div>
-    </form>
+    </div>
   );
 }
--- app/verbs/page.tsx
+++ app/verbs/page.tsx
@@ -1,25 +1,5 @@
 import VerbConj from "../components/verb-conj/VerbConj";
 
-const intialFormState = {
-  present: {
-    yo: "",
-    tu: "",
-    el: "",
-    nosotros: "",
-    vosotros: "",
-    ellos: "",
-  },
-  past: {
-    yo: "",
-    tu: "",
-    el: "",
-    nosotros: "",
-    vosotros: "",
-    ellos: "",
-  },
-  gerund: "",
-};
-
 export default function Home() {
   return (
     <main className="px-4 py-10 bg-orange-100 min-h-screen">

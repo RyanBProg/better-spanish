0bc80e3e0c042d45688a268d60465cdde26b66ff
Ryan <ryanbprog@gmail.com>
2024-09-10 11:25:55 -0500

    added functionality to control buttons

--- app/components/verb-grid/VerbGrid.tsx
+++ app/components/verb-grid/VerbGrid.tsx
@@ -9,7 +9,7 @@ import {
 } from "react-hook-form";
 import verbData from "../../data/verbs.json";
 import VerbInput, { TVerb } from "./VerbInput";
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 
 export default function VerbGrid() {
   const {
@@ -62,10 +62,11 @@ export default function VerbGrid() {
 
   const getNewRandomVerb = () => {
     setVerbIndex(Math.floor(Math.random() * verbData.length));
+    setShowAnswers(false);
     reset();
   };
 
-  const resetVerb = () => {
+  const handleResetVerb = () => {
     reset();
   };
 
@@ -142,7 +143,12 @@ export default function VerbGrid() {
           </form>
         </div>
 
-        <Controls />
+        <Controls
+          setShowAnswers={setShowAnswers}
+          showAnswers={showAnswers}
+          getNewRandomVerb={getNewRandomVerb}
+          verbList={verbData.map((verb) => verb.spanish)}
+        />
       </div>
     </>
   );
@@ -212,17 +218,34 @@ function InnerGrid({
   );
 }
 
-function Controls() {
+type ControlProps = {
+  setShowAnswers: Dispatch<SetStateAction<boolean>>;
+  showAnswers: boolean;
+  getNewRandomVerb: () => void;
+  verbList: string[];
+};
+
+function Controls({
+  setShowAnswers,
+  showAnswers,
+  getNewRandomVerb,
+  verbList,
+}: ControlProps) {
   return (
     <div className="bg-white grid grid-cols-2 grid-rows-2 md:grid-cols-3 md:grid-rows-1 gap-2 pt-2 sm:pt-3 md:pt-4 border-t-2">
       <select className="col-span-2 md:col-span-1 rounded-md w-full shadow-sm p-2 border">
-        <option value="">Select a new verb</option>
+        {verbList.map((verb) => (
+          <option value={verb}>{verb}</option>
+        ))}
       </select>
-      <button className="bg-neutral-700 w-full text-white py-2 rounded-md hover:brightness-150 transition-[filter] duration-200">
-        Show Answers
+      <button
+        onClick={() => setShowAnswers((prev) => !prev)}
+        className="bg-neutral-700 w-full text-white py-2 rounded-md hover:brightness-150 transition-[filter] duration-200">
+        {showAnswers ? "Hide Answers" : "Show Answers"}
       </button>
       <button
         type="button"
+        onClick={getNewRandomVerb}
         className="bg-neutral-700 w-full text-white py-2 rounded-md hover:brightness-150 transition-[filter] duration-200">
         Random Verb
       </button>

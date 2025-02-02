1ce9090beadb22fca2a5b1fe5e2aa845262dfe1a
Ryan <ryanbprog@gmail.com>
2025-02-02 20:40:32 +0800

    fixed deployment issue with images

--- app/learning/cheat-sheets/page.tsx
+++ app/learning/cheat-sheets/page.tsx
@@ -1,14 +1,14 @@
-import colorsImage from "../../public/images/cheat-sheets/colors/spanish-colors-colores.jpg";
-import travelImage from "../../public/images/cheat-sheets/travel/phrases-travel.jpg";
-import bodyPartsImage from "../../public/images/cheat-sheets/body/body-parts.jpg";
-import locationImage from "../../public/images/cheat-sheets/direction/location.jpg";
-import directionsImage from "../../public/images/cheat-sheets/direction/directions.jpg";
-import feelingsImage from "../../public/images/cheat-sheets/conversational/feelings.jpg";
-import greetingsImage from "../../public/images/cheat-sheets/conversational/greetings.jpg";
-import bathroomItemsImage from "../../public/images/cheat-sheets/items/bathroom-items.jpg";
-import clothingItemsImage from "../../public/images/cheat-sheets/items/clothing-items.jpg";
-import householdItemsImage from "../../public/images/cheat-sheets/items/household-items.jpg";
-import kitchenItemsImage from "../../public/images/cheat-sheets/items/kitchen-items.jpg";
+import colorsImage from "@/public/images/cheat-sheets/colors/spanish-colors-colores.jpg";
+import travelImage from "@/public/images/cheat-sheets/travel/phrases-travel.jpg";
+import bodyPartsImage from "@/public/images/cheat-sheets/body/body-parts.jpg";
+import locationImage from "@/public/images/cheat-sheets/direction/location.jpg";
+import directionsImage from "@/public/images/cheat-sheets/direction/directions.jpg";
+import feelingsImage from "@/public/images/cheat-sheets/conversational/feelings.jpg";
+import greetingsImage from "@/public/images/cheat-sheets/conversational/greetings.jpg";
+import bathroomItemsImage from "@/public/images/cheat-sheets/items/bathroom-items.jpg";
+import clothingItemsImage from "@/public/images/cheat-sheets/items/clothing-items.jpg";
+import householdItemsImage from "@/public/images/cheat-sheets/items/household-items.jpg";
+import kitchenItemsImage from "@/public/images/cheat-sheets/items/kitchen-items.jpg";
 
 import Dropdown from "../../components/dropdown/Dropdown";
 

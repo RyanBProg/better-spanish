9772bc0b2bb95ac236e3e801231e65df122ca35c
Ryan <ryanbprog@gmail.com>
2025-02-07 10:53:48 +0800

    added dashboard paths to matching config

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,5 @@ export default function middleware(req: NextRequest) {
   return withAuth(req);
 }
 export const config = {
-  matcher: ["/dashboard"],
+  matcher: ["/dashboard", "/dashboard/:path*"],
 };
